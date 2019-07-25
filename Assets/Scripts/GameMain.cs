6f20eb970924fbd7371a28ff7ac2e4026e7af82b
songtiefei <song_tiefei@126.com>
2019-07-26 00:28:01 +0800

    update art

--- Assets/Scripts/GameMain.cs
+++ Assets/Scripts/GameMain.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections;
 using System.Collections.Generic;
+using DG.Tweening;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -10,13 +11,28 @@ public class GameMain : MonoBehaviour {
         IDEL,
         READY,
         GAME,
-        END,
+        LOSE,
+        WIN,
     }
 
     public Text timeTex;
     public Text scoreTex;
-    public Text stateTex;
     public Text waterLineTex;
+
+    public Animation waterLineAnim;
+
+    public Transform waterImage;
+    public Transform waterArrow;
+
+    public GameObject IdelPage;
+    public Animation IdelAnim;
+
+    public GameObject ReadyPage;
+    public Animation readyAnim;
+
+    public GameObject WinPage;
+    public GameObject LosePage;
+
     private int currentPowerNum = 0;
     private int currentWaterLine = 0;
     private int currentTime = 0;
@@ -25,13 +41,26 @@ public class GameMain : MonoBehaviour {
     private int gameWaterLine = 600;
     private BOXINGSTATE boxState = BOXINGSTATE.IDEL;
 
+    private int waterMinY = -440;
+    private int waterMaxY = -198;
+    private float waterOffsetY = 0.4f;
+    private float waterArrowMinY = -400;
+
     private Queue<int> receivePower = new Queue<int> ();
+
+    private DOTween waterDT;
+    private DOTween waterArrowDT;
+
+    private Sequence mWaterScoreSeq;
 
     /// <summary>
     /// Awake is called when the script instance is being loaded.
     /// </summary>
     void Awake () {
         boxingNet.delegatePower = ReceiveBoxing;
+        InitPage ();
+        mWaterScoreSeq = DOTween.Sequence ();
+        mWaterScoreSeq.SetAutoKill (false);
     }
 
     void Update () {
@@ -44,11 +73,24 @@ public class GameMain : MonoBehaviour {
         currentPowerNum = 0;
         currentTime = 0;
         currentWaterLine = 0;
-        timeTex.text = "Time : 00:30";
-        scoreTex.text = "Num : " + currentPowerNum.ToString ();
-        stateTex.text = "State : idel";
+        timeTex.text = "00:30";
+        scoreTex.text = currentPowerNum.ToString ();
         boxState = BOXINGSTATE.IDEL;
-        waterLineTex.text = "Water Line : " + currentWaterLine.ToString ();
+        ChangePage (BOXINGSTATE.IDEL);
+        waterLineTex.text = currentWaterLine.ToString ();
+        DOTween.KillAll (true);
+        waterImage.localPosition = new Vector3 (0, waterMinY, 0);
+        waterArrow.localPosition = new Vector3 (776, waterArrowMinY, 0);
+        waterLineAnim.Stop ();
+        waterLineAnim.transform.localPosition = Vector3.zero;
+        IdelAnim.Play ("IdleLoop");
+    }
+
+    private void ChangePage (BOXINGSTATE state) {
+        IdelPage.SetActive (state == BOXINGSTATE.IDEL);
+        ReadyPage.SetActive (state == BOXINGSTATE.READY);
+        LosePage.SetActive (state == BOXINGSTATE.LOSE);
+        WinPage.SetActive (state == BOXINGSTATE.WIN);
     }
 
     public void ReceiveBoxing (int power) {
@@ -60,9 +102,11 @@ public class GameMain : MonoBehaviour {
             ReadyGame ();
         } else if (boxState == BOXINGSTATE.GAME) {
             currentPowerNum += 1;
-            scoreTex.text = "Num : " + currentPowerNum;
-            currentWaterLine += power;
-            RefreshWaterLine ();
+            scoreTex.text = currentPowerNum.ToString ();
+            int oldVlaue = currentWaterLine;
+            currentWaterLine += (power * 2);
+            RefreshWaterLine (oldVlaue, currentWaterLine);
+
         }
     }
 
@@ -71,63 +115,67 @@ public class GameMain : MonoBehaviour {
         StartCoroutine (PlayReadyEffect ());
     }
     IEnumerator PlayReadyEffect () {
-        stateTex.text = "3";
+        IdelAnim.Play ("IdleBegin");
         yield return new WaitForSeconds (1);
-        stateTex.text = "2";
-        yield return new WaitForSeconds (1);
-        stateTex.text = "1";
-        yield return new WaitForSeconds (1);
+        ChangePage (boxState);
+        readyAnim.Play ("readyAnim");
+        yield return new WaitForSeconds (3.5f);
         BeginGame ();
     }
 
     public void BeginGame () {
-        stateTex.text = "Play Game";
         InvokeRepeating ("RefreshTime", 0, 1.0f);
-        InvokeRepeating ("SubWaterLine", 1, 5.0f);
+        InvokeRepeating ("SubWaterLine", 1, 30.0f);
         boxState = BOXINGSTATE.GAME;
+        ChangePage (boxState);
+        waterLineAnim.Play ("waterLineAnim1");
     }
     public void SubWaterLine () {
+        int oldVlaue = currentWaterLine;
         if (currentWaterLine - subWaterLine < 0) {
             currentWaterLine = 0;
         } else {
             currentWaterLine -= subWaterLine;
         }
-        RefreshWaterLine ();
+        if (oldVlaue != currentWaterLine)
+            RefreshWaterLine (oldVlaue, currentWaterLine);
     }
     public void RefreshTime () {
         if (currentTime >= gameTime) {
-            GameEnd ();
+            GameEnd (false);
             return;
         }
         currentTime += 1;
         TimeSpan ts = TimeSpan.FromSeconds ((gameTime - currentTime));
-        timeTex.text = "Time : " + ToStringMinute (ts);
+        timeTex.text = ToStringMinute (ts);
     }
-    public void RefreshWaterLine () {
+    public void RefreshWaterLine (int oldvalue, int newvalue) {
         if (currentWaterLine >= gameWaterLine) {
             currentWaterLine = gameWaterLine;
             waterLineTex.text = currentWaterLine.ToString ();
-            GameEnd ();
+            GameEnd (true);
             return;
         }
-        waterLineTex.text = "water line : " + currentWaterLine.ToString ();
+        Debug.Log("~~~~~~~~~~~~~~~~~~~~~");
+        Debug.Log("oldvalue " + oldvalue + " newValue " + newvalue);
+        mWaterScoreSeq.Append (DOTween.To (delegate (float value) {            
+            waterLineTex.text = Mathf.CeilToInt(value).ToString();
+        }, oldvalue, newvalue, 0.4f));
+        //waterLineTex.text = currentWaterLine.ToString ();
+        waterImage.DOLocalMoveY (waterMinY + currentWaterLine * waterOffsetY, 0.2f);
+        waterArrow.DOLocalMoveY (waterArrowMinY + currentWaterLine * waterOffsetY, 0.2f);
     }
-    public void GameEnd () {
+    public void GameEnd (bool win) {
         CancelInvoke ("RefreshTime");
         CancelInvoke ("SubWaterLine");
-        boxState = BOXINGSTATE.END;
-        stateTex.text = "Game End";
+        boxState = win?BOXINGSTATE.WIN : BOXINGSTATE.LOSE;
+        ChangePage (boxState);
+        waterLineAnim.Stop ();
         StartCoroutine (GameEndEffect ());
     }
 
     IEnumerator GameEndEffect () {
-        yield return new WaitForSeconds (1.0f);
-        stateTex.text = "Game End 3";
-        yield return new WaitForSeconds (1.0f);
-        stateTex.text = "Game End 2";
-        yield return new WaitForSeconds (1.0f);
-        stateTex.text = "Game End 1";
-        yield return new WaitForSeconds (1.0f);
+        yield return new WaitForSeconds (3.0f);
         InitPage ();
     }
 
